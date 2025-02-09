8a89b7b75a02704bdced65a4589ad9cdb8173f48
Milko Dinev <mjdinevpro@gmail.com>
2025-02-09 18:51:09 +0200

    Create Models Note, Descriptions and Notes Rel

---
+++ NotesWaveAPI/NotesWave.Data.Models/Note/Note.cs
@@ -0,0 +1,35 @@
+﻿namespace NotesWave.Data.Models.Note
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Note
+    {
+        public Note()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Notes = new HashSet<NoteRels>();
+            this.NoteDescriptions = new HashSet<NoteDescription>();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+
+        public virtual ICollection<NoteRels> Notes { get; set; }
+
+        public virtual ICollection<NoteDescription> NoteDescriptions { get; set; }
+    }
+}
---
+++ NotesWaveAPI/NotesWave.Data.Models/Note/NoteDescription.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesWave.Data.Models.Note
+{
+    using DescModel = NotesWave.Data.Models.Description;
+    public class NoteDescription
+    {
+        public string NoteID { get; set; }
+        public Note Note { get; set; }
+
+        public string DescriptionID { get; set; }
+        public DescModel.Description Description { get; set; }
+    }
+}
