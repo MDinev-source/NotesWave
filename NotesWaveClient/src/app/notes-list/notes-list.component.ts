c47a3fe6f128ef55f1767c599ce92dd93cfdbfd1
Milko Dinev <mjdinevpro@gmail.com>
2025-02-12 15:42:24 +0200

    Implement notes list fetch

---
+++ NotesWaveClient/src/app/notes-list/notes-list.component.ts
@@ -0,0 +1,27 @@
+import { Note } from '../models/Note';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../services/note.service';
+
+@Component({
+  selector: 'app-notes-list',
+  imports: [],
+  templateUrl: './notes-list.component.html',
+  styleUrl: './notes-list.component.css'
+})
+export class NotesListComponent implements OnInit {
+    notes: Array<Note> = [];
+
+    constructor (private noteService: NoteService) {}
+
+    ngOnInit() {
+      this.fetchNotes();
+    }
+
+    fetchNotes(){
+      this.noteService
+      .getNotes()
+      .subscribe(notes => {
+          this.notes = notes
+        })
+    }
+}
