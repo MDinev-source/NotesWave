9a8dbe499c95718cbe8d6303a00ae3c6cfb43393
Milko Dinev <mjdinevpro@gmail.com>
2025-02-19 13:16:03 +0200

    Migration Note State and Description Type Editable

---
+++ NotesWaveAPI/Data/Migrations/20250219111527_NotesState_DescType_Editable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesWave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotesState_DescType_Editable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Notes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Descriptions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Descriptions");
+        }
+    }
+}
